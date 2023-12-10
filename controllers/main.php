63b65b1f118c2734e8e3b14fe820de2de4baf0f5
Mabel <mauratavara287@gmail.com>
2023-12-09 21:05:41 -0500

    Estadisticas en el main

--- controllers/main.php
+++ controllers/main.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Libs\Session;
+use Models\MainModel;
 
 class Main extends Session
 {
@@ -14,5 +15,56 @@ class Main extends Session
   public function render()
   {
     $this->view->render('main/index');
+  }
+
+  public function getVentasPorMes()
+  {
+    $main = new MainModel();
+    $totales = $main->getVentasPorMes('2023');
+    $meses = [];
+    $data = [];
+
+    foreach ($totales as $total) {
+      $data[] = intval($total['total']);
+      $meses[] = $this->meses()[$total['mes']];
+    }
+
+    $series = [[
+      "name" => "Cantidad de ventas",
+      "data" => $data
+    ],];
+    $this->response(["series" => $series, "categories" => $meses]);
+  }
+  public function getTotalVentasPorMes()
+  {
+    $main = new MainModel();
+    $totales = $main->getTotalVentasPorMes('2023');
+    $meses = [];
+    $data = [];
+
+    foreach ($totales as $total) {
+      $data[] = intval($total['total_por_mes']);
+      $meses[] = $this->meses()[$total['mes']];
+    }
+
+    $series = [[
+      "name" => "Total de ventas",
+      "data" => $data
+    ],];
+    $this->response(["series" => $series, "categories" => $meses]);
+  }
+  public function getCantidadTipoPedido()
+  {
+    $main = new MainModel();
+    $totales = $main->getCantidadTipoPedido();
+    $labels = [];
+    $series = [];
+
+    foreach ($totales as $total) {
+      $labels[] = ucfirst($total['tipo']);
+      $series[] = $total['total'];
+    }
+
+    $this->response(["series" => $series, "labels" => $labels]);
   }
 }
